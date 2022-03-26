9698780fbc0df2fe886324b6c40b757500e143ff
Priyansh Malik <priyanshmalik332@gmail.com>
2022-03-26 20:28:32 +0530

    3rd file

---
+++ LearnJava/src/P3_Task02_ConstantsInJava.java
@@ -0,0 +1,14 @@
+
+public class P3_Task02_ConstantsInJava {
+
+	// final double pi = 3.14; We can't access the non-static members int the static function
+	 static final int x = 4;
+	public static void main(String[] args) {
+		final double pi = 3.14;
+		System.out.println(pi);
+		System.out.println(x);
+	
+		
+	}
+
+}
